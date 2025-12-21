9606093118319be42d959ad7901d1d8984a046aa
Denys Yerokhov <st72861@upce.cz>
2025-12-21 19:30:29 +0100

    Добавьте файлы проекта.

---
+++ UltimateCleaner/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace UltimateCleaner
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
